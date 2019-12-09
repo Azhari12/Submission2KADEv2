a10dc759524adf75ba60eb855e94da124821a74d
Azhari12 <49281805+Azhari12@users.noreply.github.com>
2019-12-09 13:02:36 +0700

    Update Next.kt

--- app/src/main/java/com/example/fottballmatchschedule2/Model/Next.kt
+++ app/src/main/java/com/example/fottballmatchschedule2/Model/Next.kt
@@ -3,6 +3,6 @@ package com.example.fottballmatchschedule2.Model
 import com.google.gson.annotations.SerializedName
 
 data class Next(
-    @SerializedName("strSport")
+    @SerializedName("strEvent")
     var matchEvent: String? = null
 )
